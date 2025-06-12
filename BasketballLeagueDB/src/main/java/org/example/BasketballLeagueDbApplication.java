3048670a8769a1e41237ac78e26d36698865ebb8
jansarba <jansarbasarba@gmail.com>
2025-06-12 16:30:03 +0200

    oj ale merge conlict wydupi!

---
+++ BasketballLeagueDB/src/main/java/org/example/BasketballLeagueDbApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballLeagueDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketballLeagueDbApplication.class, args);
+    }
+}
